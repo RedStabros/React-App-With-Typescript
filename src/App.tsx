8ea5048520ccc62805e08d690c95461846551138
MauricioSG <udcarkangel@gmail.com>
2022-07-02 23:29:48 -0500

    First commit app React with Typescript for Subs

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import List from "./components/List";
+
+interface Sub {
+  nick: string;
+  subMonths: number;
+  avatar: string;
+  description?: string;
+}
+
+interface AppState {
+  subs: Array<Sub>;
+  newSubsNumber: number;
+}
+
+const INITIAL_STATE = [
+  {
+    nick: "dapelu",
+    subMonths: 3,
+    avatar: "https://i.pravatar.cc/150?u=dapelu",
+    description: "Dapelu es moderador",
+  },
+  {
+    nick: "sergio_serrano",
+    subMonths: 7,
+    avatar: "https://i.pravatar.cc/150?u=sergio_serrano",
+  },
+];
+
+function App() {
+  const [subs, setSubs] = useState<AppState["subs"]>([]);
+  const [newSubsNumber, setNewSubsNumber] =
+    useState<AppState["newSubsNumber"]>(0);
+
+  useEffect(() => {
+    setSubs(INITIAL_STATE);
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Midu subs</h1>
+      <List subs={subs} />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react"
+
+const Form = () => {
+
+    const [inputValues, setInputValues] = useState({
+        nick: '',
+        subMonths: 0,
+        avatar: '',
+        description: ''
+    })
+
+    const handleSubmit = () => {}
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setInputValues({
+            ...inputValues,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input onChange={handleChange} value={inputValues.nick} type='text' name='nick' placeholder="nick" />
+                <input onChange={handleChange} value={inputValues.subMonths} type='number' name='subMonths' placeholder="subMonths" />
+                <input onChange={handleChange} value={inputValues.avatar} type='text' name='avatar' placeholder="avatar" />
+                <textarea onChange={handleChange} value={inputValues.description} name='nick' placeholder="description" />
+                <button>Save new sub!</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form
